1e425cd74da927f8c3f054ed2031a373cd6286a7
Fixat1on <33039102+Alexhecker@users.noreply.github.com>
2020-02-03 11:04:27 -0800

    this

---
+++ AsciiArt.java
@@ -0,0 +1,31 @@
+package Unit0;
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name - Alexander Hecker
+//Date - 1/31/2020
+//Class - APCSA - Remington
+//Lab  -  AsciiArt
+
+public class AsciiArt
+{
+	public static void main ( String[] args )
+	{
+		System.out.println("Alexander Hecker \n\n" );
+		System.out.println("What type of ANIMAL will you draw?" );
+		System.out.println("\n\n\n\n" );
+
+		System.out.println("          /\\_/\\    " );
+		System.out.println("          >^.^<.---.   " );
+		System.out.println("         _'-`-'     )\\    " );
+		System.out.println("        (6--\\ |--\\ (`.`-. " );
+		System.out.println("            --'  --'  ``-'    " );
+		//add other output
+
+		System.out.println(" \n\n\n\nHelpFul Hints" );
+		System.out.println("\\\\ draws one backslash on the screen!\n" );
+		System.out.println("\\\" draws one double quote on the screen!\n" );
+		System.out.println("\\\' draws one single quote on the screen!\n" ); 
+	}
+}
+//hi
