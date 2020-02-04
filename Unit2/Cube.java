dee5850effe6493cc1ed114a1819dff6d48f67b3
Fixat1on <33039102+Alexhecker@users.noreply.github.com>
2020-02-04 10:02:15 -0800

    Create Cube.java

---
+++ Unit2/Cube.java
@@ -0,0 +1,13 @@
+//(c) A+ Computer Science
+// www.apluscompsci.com
+//Name - Alexander Hecker
+//Date - 2/4/2020
+
+public class Cube
+{
+	public static double area( int side )
+	{ 
+		
+		return ((double) side * side * 6);
+	}
+}
