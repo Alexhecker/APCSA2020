4c635f5093b851038e0b953b02964ef2450e5056
Fixat1on <33039102+Alexhecker@users.noreply.github.com>
2020-02-04 10:04:15 -0800

    Create LineRunner.java

---
+++ Unit2/LineRunner.java
@@ -0,0 +1,19 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name - Alexander Hecker
+//Date - 2/4/2020
+//Class - APCSA
+//Lab  - LineRUnner
+
+public class LineRunner
+{
+   public static void main( String[] args )
+   {
+		System.out.printf( "Slope is :: %.2f\n" , Line.getSlope( 1,9,14,2 ) );
+		System.out.printf( "Slope is :: %.2f\n" , Line.getSlope( 1,7,18,3 ) );
+		System.out.printf( "Slope is :: %.2f\n" , Line.getSlope( 6,4,2,2 ) );
+		System.out.printf( "Slope is :: %.2f\n" , Line.getSlope( 4,4,5,3 ) );
+		System.out.printf( "Slope is :: %.2f\n" , Line.getSlope( 1,1,2,9 ) );
+		System.out.printf( "Slope is :: %.2f\n" , Line.getSlope( 1,7,2,9 ) );
+		
+	}
+}
