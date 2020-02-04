7091dff85aa0837d69bc8978cba1c753e0d1a08d
Fixat1on <33039102+Alexhecker@users.noreply.github.com>
2020-02-04 10:03:21 -0800

    Create Line.java

---
+++ Unit2/Line.java
@@ -0,0 +1,13 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name - Alexander Hecker
+//Date -2/4/2020
+
+public class Line
+{
+	public static double getSlope( int x1, int y1, int x2, int y2 )
+	{
+		return ((double) (y2 - y1)/ (x2 - x1));
+	}
+
+}
