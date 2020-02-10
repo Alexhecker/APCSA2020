46da97f86baa92e34d42d868869312acd3601fdf
Fixat1on <33039102+Alexhecker@users.noreply.github.com>
2020-02-10 10:51:02 -0800

    Create NumberVerify.java

---
+++ Unit4/NumberVerify.java
@@ -0,0 +1,23 @@
+package Unit4;
+//(c) A+ Computer Science
+
+//www.apluscompsci.com
+
+//Name - Alexander Hecker
+//Date -  2/10/2020
+//Class - APCSA
+//Lab  - NumberVerify.java
+
+import static java.lang.System.*;
+
+public class NumberVerify
+{
+	public static boolean isOdd( int num )
+	{
+		return num % 2 != 0;
+	}
+	public static boolean isEven( int num )
+	{
+		return num % 2 == 0;
+	}	
+}
