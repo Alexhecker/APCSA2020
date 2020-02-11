ccbc8aaf9117cf107703ea760fa9fe3d10efa96f
Fixat1on <33039102+Alexhecker@users.noreply.github.com>
2020-02-11 10:57:39 -0800

    Create AvgLen.java

---
+++ Unit5/AvgLen.java
@@ -0,0 +1,19 @@
+package Unit5;
+
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name - Alexander Hecker
+//Date - 2/11/2020
+
+public class AvgLen
+{
+   public static double go( String a, String b )
+	{
+	   int lengthA = a.length();
+	   int lengthB = b.length();
+	   
+	   
+	   
+		return (double) (lengthA + lengthB) / 2;
+	}
+}
