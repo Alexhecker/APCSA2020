bf696ffbe37d52c08a70e108bbe3c5c38d66c0db
Fixat1on <33039102+Alexhecker@users.noreply.github.com>
2020-02-12 11:45:30 -0800

    Create LetterRemover.java

---
+++ Unit6/LetterRemover.java
@@ -0,0 +1,50 @@
+package Unit6;
+
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name - Alexander Hecker
+
+import static java.lang.System.*;
+
+public class LetterRemover
+{
+   private String sentence;
+   private char lookFor;
+
+	public LetterRemover()
+	{
+		sentence = null;
+		lookFor = ' ';
+	}
+
+	//add in second constructor
+	
+	public LetterRemover(String sentence, char lookFor)
+	{
+		this.sentence = sentence;
+		this.lookFor = lookFor;
+	}
+	
+	
+	public void setRemover(String s, char rem)
+	{
+		sentence = s;
+		lookFor = rem;
+	}
+
+	public String removeLetters()
+	{
+		String cleaned=sentence;
+		int loc = cleaned.indexOf(lookFor);
+		while(loc > -1) {
+			cleaned = cleaned.substring(0, loc) + cleaned.substring(loc + 1);
+			loc = cleaned.indexOf(lookFor);
+		}
+		return cleaned;
+	}
+
+	public String toString()
+	{
+		return sentence + " - letter to remove " + lookFor + "\n" + removeLetters() + "\n\n";
+	}
+}
