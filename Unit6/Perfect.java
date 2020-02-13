d0833776a270915ff33d929ea6f36c46b03188e2
Fixat1on <33039102+Alexhecker@users.noreply.github.com>
2020-02-13 10:21:36 -0800

    Create Perfect.java

---
+++ Unit6/Perfect.java
@@ -0,0 +1,57 @@
+package Unit6;
+
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+import static java.lang.System.*;
+
+public class Perfect
+{
+   private int number;
+   
+   public Perfect()
+   {
+	   number = 0;
+   }
+   
+   public Perfect(int number)
+   {
+	   this.number = number;
+   }
+
+   public void setNumber(int number)
+   {
+	   this.number = number;
+   }
+
+	public boolean isPerfect()
+	{
+		int sum = 0;
+		int i;
+		for (i=1;i<number;i++)
+		{
+			if (number%i == 0)
+			{
+				sum += i;
+			}
+		}
+		
+		if (sum == number)
+		{
+			return true;
+		}
+		return false;
+	}
+
+	public String toString()
+	{
+		if (isPerfect())
+		{
+			return number + " is perfect.\n";
+		} else {
+			return number + " is not perfect.\n";
+		}
+	}
+	
+}
