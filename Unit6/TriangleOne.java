abc6fda6fc118b33cf3ddcd6439485526889956c
Fixat1on <33039102+Alexhecker@users.noreply.github.com>
2020-02-13 10:22:06 -0800

    Create TriangleOne.java

---
+++ Unit6/TriangleOne.java
@@ -0,0 +1,39 @@
+package Unit6;
+
+//(c) A+ Computer Science
+
+//www.apluscompsci.com
+//Name - Alexander Hecker
+
+import static java.lang.System.*;
+
+public class TriangleOne
+{
+	private String word;
+
+	public TriangleOne()
+	{
+		word = "";
+	}
+
+	public TriangleOne(String s)
+	{
+		word = s;
+	}
+
+	public void setWord(String s)
+	{
+		word = s;
+	}
+
+	public void print( )
+	{
+		//the assignment seems to want me to print in the class,
+		// so this is what I'm doing.
+		int i;
+		for (i=0;i < word.length();i++) {
+			out.println(word.substring(0,word.length()-i));
+		}
+		out.println();
+	}
+}
