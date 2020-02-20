ecf209b66979bb1db47f797551063dbe316f0aad
Fixat1on <33039102+Alexhecker@users.noreply.github.com>
2020-02-20 11:06:26 -0800

    Create TriplesRunner.java

---
+++ Unit7/TriplesRunner.java
@@ -0,0 +1,16 @@
+package Unit7;
+
+//(c) A+ Computer Science
+// www.apluscompsci.com
+//Name - Alexander Hecker
+
+import static java.lang.System.*;
+
+public class TriplesRunner
+{
+   public static void main(String args[])
+   {
+	   Triples test = new Triples(110);
+	   System.out.println(test);
+	}
+}
