3fcb037b5782ced9e4604e68166460d9ef02c123
Fixat1on <33039102+Alexhecker@users.noreply.github.com>
2020-03-03 09:57:24 -0800

    Create TriangleWordRunner.java

---
+++ Unit11/TriangleWordRunner.java
@@ -0,0 +1,45 @@
+package Unit11;
+
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name - Alexander Hecker
+
+import java.util.Scanner;
+import static java.lang.System.*;
+
+public class TriangleWordRunner
+{
+	public static void main( String args[] )
+	{
+		Scanner keyboard = new Scanner(System.in);
+	   	String choice="";
+		do{
+			String a = ("a");	
+			out.println("\nEnter a word :: " + a);
+			TriangleWord.printTriangle(a);
+			
+			String it = ("it");	
+			out.println("\nEnter a word :: " + it);
+			TriangleWord.printTriangle(it);
+			
+			String box = ("box");	
+			out.println("\nEnter a word :: " + box);
+			TriangleWord.printTriangle(box);
+			
+			String toad = ("toad");	
+			out.println("\nEnter a word :: " + toad);
+			TriangleWord.printTriangle(toad);
+			
+			String fishy = ("fishy");	
+			out.println("\nEnter a word :: " + fishy);
+			TriangleWord.printTriangle(fishy);
+			
+			String dog = ("dog");	
+			out.println("\nEnter a word :: " + dog);
+			TriangleWord.printTriangle(dog);
+
+			System.out.print("\nDo you want to enter more sample input? ");
+			choice=keyboard.next();			
+		}while(choice.equals("Y")||choice.equals("y"));
+	}
+}
