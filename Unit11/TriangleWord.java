c777eb96a9ccd25bf950238367541f9f5dc60f70
Fixat1on <33039102+Alexhecker@users.noreply.github.com>
2020-03-03 09:56:56 -0800

    Create TriangleWord.java

---
+++ Unit11/TriangleWord.java
@@ -0,0 +1,29 @@
+package Unit11; 
+
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name - Alexander Hecker
+
+import java.util.Scanner;
+import static java.lang.System.*;
+
+public class TriangleWord
+{
+	//instance variables and constructors could be present, but are not necessary
+		
+	public static void printTriangle(String word)
+	{
+		int letter = 1;
+		
+		for (int i = 0; i < word.length(); i++)
+		{
+			String tempOutput = "";
+			
+			for (int j = 0; j<letter; j++) {
+				tempOutput += word.substring(0, letter);
+			}
+			letter ++;
+			out.println(tempOutput);
+		}
+	}
+}
